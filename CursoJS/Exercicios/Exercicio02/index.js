ab8b53f8fcd0b318d7dfa513a8f9893e9c035339
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-01-08 17:02:15 -0300

    Exercicio02

---
+++ CursoJS/Exercicios/Exercicio02/index.js
@@ -0,0 +1,10 @@
+const meuPrimeiroNome = 'João Marcos'
+const meuSegundoNome = 'Pimenta Martins'
+let minhaIdade = 19
+const dataDeNascimento = '27/09/2002'
+let meuPeso = 84
+let minhaAltura = 1.65
+let meuIMC = meuPeso/(minhaAltura*minhaAltura)
+
+console.log('Meu nome é', meuPrimeiroNome, meuSegundoNome,'tenho', minhaIdade)
+console.log(`e nasci em ${dataDeNascimento} tenho ${minhaAltura}CM de altura peso ${meuPeso}Kg e meu IMC é ${meuIMC}`)
