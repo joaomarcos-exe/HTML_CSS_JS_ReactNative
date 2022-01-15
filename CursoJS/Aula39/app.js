2f6218a7d11743667ff084f3dc853ef776d31cfb
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-01-15 17:36:36 -0300

    Aula39

---
+++ CursoJS/Aula39/app.js
@@ -0,0 +1,12 @@
+//Utilizando && e ||
+function test() {
+    return 'Ola'
+};
+
+let algo = false
+
+let algo2 = false
+
+console.log(algo && algo2 && test());
+
+console.log(algo || algo2 || test());
