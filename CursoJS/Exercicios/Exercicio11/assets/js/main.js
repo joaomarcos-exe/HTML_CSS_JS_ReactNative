99e381bfa29cf58351b3885063041cdb64e99b4b
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-01-20 20:50:04 -0300

    Exercicio11

---
+++ CursoJS/Exercicios/Exercicio11/assets/js/main.js
@@ -0,0 +1,12 @@
+const paragrafo = document.querySelector('.paragrafo')
+const ps = paragrafo.querySelectorAll('p')
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorContainer = estilosBody.backgroundColor;
+
+for (let p of ps) {
+    p.style.backgroundColor = backgroundColorContainer
+    p.style.color = '#FFFF'
+}
+
+
