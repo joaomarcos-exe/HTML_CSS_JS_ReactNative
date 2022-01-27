13b6ae7642bc0ed736698c753d66177d176d70f9
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-01-26 23:43:05 -0300

    .

--- CursoJS/Exercicios/Exercicio13/assets/js/app.js
+++
@@ -1,5 +0,0 @@
-const oclok = document.querySelector('#oclok')
-const start = document.querySelector('#start')
-const pause = document.querySelector('#pause')
-const reset = document.querySelector('#reset')
-
