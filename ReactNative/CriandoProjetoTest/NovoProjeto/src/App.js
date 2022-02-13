582dbb04899189223c99d112dec87fc830267576
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-02-13 16:57:59 -0300

    Update App.js

--- ReactNative/CriandoProjetoTest/NovoProjeto/src/App.js
+++ ReactNative/CriandoProjetoTest/NovoProjeto/src/App.js
@@ -1,8 +1,13 @@
 import React from "react"
-import {View} from 'react-native'
+import {View, StyleSheet} from 'react-native'//Invocamos o styleShet para usarmos estilos
 
-import X, { Comp1 , Comp2 } from './components/Multi'
-import BlaBla from './components/Primeiro'
+import Botao from './components/Botao'
+//import MinMax from "./components/MinMax"
+// import X, { Comp1 , Comp2 } from './components/Multi'
+// import BlaBla from './components/Primeiro'
+// import Aleatorio from './components/Aleatorio'
+//import Titulo from './components/Titulo'
+
 
 // function App(){
 //     //return <Text>Valor textual</Text>
@@ -14,13 +19,33 @@ import BlaBla from './components/Primeiro'
 //<View>Alguma coisa</View>
 
 
-//export default () => <View><BlaBla></BlaBla></View>
 
 export default () => (
-    <View>
+    //Aqui eu ligo o estilo criado la em baixo ao meu View
+    //O primeiro MinMax estamos passando valores string e o segundo valores numericos
+    <View style={style.App}>
+        <Botao/>
+        {/* <MinMax max='30' min='20'/>
+        <MinMax max={100} min={50}/> */}
+        {/* <X/>    
+        <Titulo principal="Cadastro de produto"
+            secundario="Tela de cadastro do produto"
+        />
         <Comp1/>
         <Comp2/>
-        <X/>
+        <BlaBla></BlaBla>
+        <Aleatorio min={30} max= {100}/> */}
+        
 
     </View>
-    )
+)
+
+const style = StyleSheet.create({//Aqui criamos os estilo como por exemplo irei criar um estilo para o App
+    App:{
+        flex: 1,//Esse flex e a versão resumida e o 1 significa que o container pode crescer e ocupar a tela toda e na vdd esse é do flexGrow
+        justifyContent: "center",
+        alignItems: 'center',
+        padding: 20
+        
+    }
+})
