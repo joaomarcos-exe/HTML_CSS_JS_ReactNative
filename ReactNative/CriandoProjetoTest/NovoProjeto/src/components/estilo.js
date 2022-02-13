e5b5167627ecf9751ed4b43ca915bb9b2361a66d
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-02-13 16:56:01 -0300

    Style
    Criando estilos para texto.

---
+++ ReactNative/CriandoProjetoTest/NovoProjeto/src/components/estilo.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native'
+export default StyleSheet.create({//Nesse arquivo posso usar para criar stilos padrões como para texto 
+    txtG: {
+        fontSize: 24,
+        textAlign: 'center'//Eu não poderia colocar no estilo App pq se não ele não seria especificamente para o Text
+    },
+    textP:{
+        fontSize: 18
+    }
+})
